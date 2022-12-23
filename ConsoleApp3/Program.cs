e6ff8ca069d55675dca4a85d4a4d9b7eb3e5ac4f
mohamedshafe <121301787+mohamedshafe@users.noreply.github.com>
2022-12-23 16:25:44 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PrimeNumberFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user for the start and end of the range
+            Console.Write("Enter the start of the range: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the end of the range: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            // Check each number in the range to see if it is prime
+            for (int i = n1; i <= n2; i++)
+            {
+                
+
+                // Check if the number is evenly divisible by any other number in the range
+                int sum =0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        ++sum;
+                        
+                    }
+                }
+
+                // If the number is prime, print it to the console
+                if (sum==2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+    class program
+{
+    static bool Isperfect(int number)
+    {
+        int sum = 0;
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0) sum += i;
+        }
+        if (sum == number) return true;
+        return false;
+
+    }
+
+    static void Main(string[] args)
+    {
+        int x, y;
+        Console.Write("Please enter the first number:");
+        x = int.Parse(Console.ReadLine());
+        Console.Write("Please enter the second number :");
+        y = int.Parse(Console.ReadLine());
+        Console.WriteLine("\n");
+        for (int i=x; i<=y; i++)
+        {
+            if(Isperfect(i)) Console.Write("{0}, ", i);
+        }
+        Console.ReadKey();
+
+
+      }
+
+
+    }
+
+
